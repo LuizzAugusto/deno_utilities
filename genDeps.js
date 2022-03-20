3ccec55db32aad1e7b52aa8e9cdb3ff7c5f12b50
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2022-03-20 20:51:35 -0300

    (genDeps.js) it won't read dir inside

--- genDeps.js
+++ genDeps.js
@@ -9,7 +9,9 @@ export async function genDeps({ ext = EXT, depsDir = DEPS_DIR } = {}) {
   await Deno.writeTextFile("deps." + ext, "")
 
   for await (const dirEntry of Deno.readDir("deps")) {
-    const file = "export * from \"" + depsDir + "/" + dirEntry.name + "\"\n"
-    await Deno.writeTextFile("deps." + ext, file, { append: true })
+    if (!dirEntry.isDirectory) {
+      const file = "export * from \"" + depsDir + "/" + dirEntry.name + "\"\n"
+      await Deno.writeTextFile("deps." + ext, file, { append: true })
+    }
   }
 }
